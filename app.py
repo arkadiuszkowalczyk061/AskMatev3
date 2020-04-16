e904d3dcc70594ed99fe0d9bb96920046988bfd4
Stasio <stachowicz.karol@gmail.com>
2020-04-16 20:20:46 +0200

    add, display question

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ def index():
     return render_template('index.html', questions=questions)
 
 
+@app.route('/add_question', methods=['POST', "GET"])
+def add_question():
+    global questions
+    if request.method == 'POST':
+        new_question = dict(request.form)
+        new_question['Title'] = request.form['Title']
+        new_question['Description'] = request.form['Description']
+
+        questions.append(new_question)
+        return redirect(url_for('display_question'))
+    else:
+        return render_template('add_question.html')
+
+
+@app.route('/display_question', methods=['POST', "GET"])
+def display_question():
+    return render_template('display_question.html', questions=questions)
+
+
 
 if __name__ == '__main__':
     app.run()
