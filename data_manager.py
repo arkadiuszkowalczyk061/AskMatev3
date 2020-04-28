4fc701a17463efa8eac9dc1084d7f3d00ee86da1
Arkadiusz Kowalczyk <akowalczyk061@gmail.com>
2020-04-28 13:37:28 +0200

    added write_data_to_questions to data_manager

--- data_manager.py
+++ data_manager.py
@@ -58,5 +58,23 @@ def get_all_question_headers(cursor):
     cursor.execute("""SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
                       WHERE TABLE_NAME = 'questions';""")
     table_headers = cursor.fetchall()
+    return (table_headers)
+
+@connection_handler
+def get_all_answer_headers(cursor):
+    cursor.execute("""SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+                      WHERE TABLE_NAME = 'answers';""")
+    table_headers = cursor.fetchall()
     return table_headers
 
+@connection_handler
+def write_data_to_questions(cursor, data_to_add):
+    cursor.execute("""INSERT INTO questions VALUE (%(id)s, %(submission_time)s, %(view_number)s, %(vote_number)s, %(title)s, %(message)s, %(image)s);""",
+                   {
+                    'id': data_to_add['id'],
+                    'submission_time': data_to_add['submission_time'],
+                    'view_number': data_to_add['view_number'],
+                    'vote_number': data_to_add['title'],
+                    'message': data_to_add['message'],
+                    'image': data_to_add['image']
+                   })
