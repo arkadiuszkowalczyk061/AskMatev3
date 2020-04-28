e9d6af290be5b3d01746ded622216e63480607ea
Arkadiusz Kowalczyk <akowalczyk061@gmail.com>
2020-04-28 13:01:43 +0200

    Added functionality to connection.py to return data as list instead of OrderedDict, data_manager rewritten to handle SQL querries

--- data_manager.py
+++ data_manager.py
@@ -1,32 +1,49 @@
-ANSWERS_PATH = 'sample_data/answer.csv'
-QUESTIONS_PATH = 'sample_data/question.csv'
-import csv
-from csv import writer
+from datetime import datetime
+from connection import connection_handler, connection_handler_list
 
-def import_questions():
-    questions = []
-    csv_file = open(QUESTIONS_PATH, 'r')
-    lines = csv.reader(csv_file)
-    for row in lines:
-        questions.append(row)
-    return questions
+# ANSWERS_PATH = 'sample_data/answer.csv'
+# QUESTIONS_PATH = 'sample_data/question.csv'
+# import csv
+# from csv import writer
+#
+# def import_questions():
+#     questions = []
+#     csv_file = open(QUESTIONS_PATH, 'r')
+#     lines = csv.reader(csv_file)
+#     for row in lines:
+#         questions.append(row)
+#     return questions
+#
+#
+# def import_answers():
+#     answers = []
+#     csv_file = open(ANSWERS_PATH, 'r')
+#     lines = csv.reader(csv_file)
+#     for row in lines:
+#         answers.append(row)
+#     return answers
+#
+#
+# def append_data(file_name, list_of_elements):
+#     with open(file_name, 'a+') as write_obj:
+#         writer=csv.writer(write_obj)
+#         writer.writerow(list_of_elements)
+#
+# def new_question(QUESTIONS_PATH, new_line):
+#     with open(QUESTIONS_PATH, 'a+', newline='') as write_obj:
+#         csv_writer = writer(write_obj)
+#         csv_writer.writerow(new_line)
 
 
-def import_answers():
-    answers = []
-    csv_file = open(ANSWERS_PATH, 'r')
-    lines = csv.reader(csv_file)
-    for row in lines:
-        answers.append(row)
+@connection_handler
+def get_all_answers(cursor):
+    cursor.execute("""SELECT * FROM answers""")
+    answers = cursor.fetchall()
     return answers
 
+@connection_handler
+def get_all_questions(cursor):
+    cursor.execute("""SELECT * FROM questions""")
+    questions = cursor.fetchall()
+    return questions
 
-def append_data(file_name, list_of_elements):
-    with open(file_name, 'a+') as write_obj:
-        writer=csv.writer(write_obj)
-        writer.writerow(list_of_elements)
-
-def new_question(QUESTIONS_PATH, new_line):
-    with open(QUESTIONS_PATH, 'a+', newline='') as write_obj:
-        csv_writer = writer(write_obj)
-        csv_writer.writerow(new_line)
