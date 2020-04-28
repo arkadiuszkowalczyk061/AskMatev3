9dc7cc4a287dd07ec4eca49b209f7e9a44a0f9df
Arkadiusz Kowalczyk <akowalczyk061@gmail.com>
2020-04-28 11:23:01 +0200

    Added additional functionality to .sql files

--- answers-sql.sql
+++ answers-sql.sql
@@ -1,1 +1,3 @@
+DROP TABLE IF EXISTS public.answers;
+CREATE TABLE public.answers (id INTEGER PRIMARY KEY, submission_time INTEGER, vote_number INTEGER, question_id INTEGER, message TEXT);
 INSERT INTO public.answers (id, submission_time, vote_number, question_id, message) VALUES (2, 1493088154, 35, 1, 'Look it up in the Python docs');
